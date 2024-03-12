f9ca4cf180d4dd7c4422823fb4358e8249bccdb5
LeightonSkervin <lskervin23@gmail.com>
2024-03-12 14:34:19 -0400

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length == 0:
+        print([])
+    elif length == 1:
+        fibonacci.append(0)
+        print([0])
+    elif length == 2:
+        fibonacci.append(0)
+        fibonacci.append(1)
+        print(fibonacci)
+    else:
+        fibonacci = [0,1]
+        for num in range(length -2):
+            fibonacci.append(fibonacci[-2] + fibonacci[-1])
+        print(fibonacci)
+
+
+    #     
+    
+
+
+    
+
+
+
+
+    
